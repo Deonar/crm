6309cf45f335d56456742fde51063d580eb5228e
deonarius <deonarius@gmail.com>
2018-03-04 00:44:11 +0200

    main page

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,11 @@
 $(function() {
 
-	// Custom JS
-
+	// Slider
+    $('.joker-slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        dots: true,
+        prevArrow:'<button type="button" class="slick-prev"></button>',
+        nextArrow:'<button type="button" class="slick-next"></button>',
+    });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ gulp.task('browser-sync', function() {
 			baseDir: 'app'
 		},
 		notify: false,
-		open: false,
+		open: true,
 		// tunnel: true,
 		// tunnel: "projectmane", //Demonstration page: http://projectmane.localtunnel.me
 	})
@@ -35,6 +35,7 @@ gulp.task('sass', function() {
 gulp.task('js', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/slick-slide/slick.min.js',
 		'app/js/common.js', // Always at the end
 		])
 	.pipe(concat('scripts.min.js'))
